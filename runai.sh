4b5ed903fd5512892f8df5c3ca6d471e5efc0d42
hojjatkarami <g.hojatkarami@gmail.com>
2023-01-05 20:10:53 +0100

    update

--- runai.sh
+++ runai.sh
@@ -13,7 +13,7 @@ TEDA__shpmarklabel="-event_enc 1 -state       -mod single    -next_mark 1  -samp
 
 
 
-DATA_NAME=" physio2019_1d_HP_std_AB"  
+DATA_NAME="physio2019_1d_HP_std_AB"  
 COMMON="-data  $PRE/$DATA_NAME/ -epoch 30 -per 100 -w_pos -batch_size 8  -lr 0.00245  -ES_pat 10 -wandb"
 
 python optuna1.py $COMMON $DA__label -user_prefix "[opt]DA__label-"&
