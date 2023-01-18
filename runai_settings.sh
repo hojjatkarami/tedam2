be337a8a39cba4cb491133f798d8348ca1362133
hojjatkarami <g.hojatkarami@gmail.com>
2023-01-18 09:24:34 +0100

    up sh

--- runai_settings.sh
+++ runai_settings.sh
@@ -21,29 +21,20 @@ TEDA__shpmark="-event_enc 1    -state       -mod single    -next_mark 1  -sample
 TEDA__label="-event_enc 1 -state       -mod none    -next_mark 0  -sample_label 1"
 
 
+DATA_NAME="p12"
+COMMON="    -epoch 50 -per 100 -w_pos -batch_size 8  -lr 0.001 -weight_decay 1  -ES_pat 20 -wandb"
 
-COMMON="    -epoch 50 -per 100 -w_pos -batch_size 8  -lr 0.00245 -weight_decay 0.1  -ES_pat 20 -wandb"
-
-
-DATA_NAME="p12" 
 
 # DA__label
 EXP=" -data  $PRE/$DATA_NAME/ -setting rand " 
-python tune_optuna.py $EXP $COMMON $DA__label -user_prefix "[T2-rand]DA__label-" &
+python tune_optuna.py $EXP $COMMON $DA__label -user_prefix "[R8-rand]DA__label-" &
 
 
-# TE__shpmark
-EXP=" -data  $PRE/$DATA_NAME/ -setting rand " 
-python tune_optuna.py $EXP $COMMON $TE__shpmark -user_prefix "[T2-rand]TE__shpmark-" &
 
+DATA_NAME="p19"
+COMMON="    -epoch 50 -per 100 -w_pos -batch_size 8 -lr 0.001 -weight_decay 0.001  -ES_pat 20 -wandb"
 
-DATA_NAME="p19" 
 
 # DA__label
 EXP=" -data  $PRE/$DATA_NAME/ -setting rand " 
-python tune_optuna.py $EXP $COMMON $DA__label -user_prefix "[T2-rand]DA__label-" &
-
-
-# TE__shpmark
-EXP=" -data  $PRE/$DATA_NAME/ -setting rand " 
-python tune_optuna.py $EXP $COMMON $TE__shpmark -user_prefix "[T2-rand]TE__shpmark-" &
+python tune_optuna.py $EXP $COMMON $DA__label -user_prefix "[R8-rand]DA__label-" &
