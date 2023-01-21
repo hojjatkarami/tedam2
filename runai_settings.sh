0c0efacb2005b66334b38e04ffedaa7e3512e008
hojjatkarami <g.hojatkarami@gmail.com>
2023-01-21 18:40:32 +0100

    sh up

--- runai_settings.sh
+++ runai_settings.sh
@@ -6,7 +6,7 @@ waitforjobs() {
 
 
 N_JOBS=5
-USER_PREFIX=V1
+USER_PREFIX=V2
 # p12     -lr 0.001 -weight_decay 0.001  
 # p19     -lr 0.001 -weight_decay 1    #DA__label   TE__shpmark
 
@@ -34,7 +34,7 @@ TEDA__ml="-event_enc 1 -state       -mod ml    -next_mark 0  -sample_label 0"
 
 
 DATA_NAME="p19"
-COMMON="    -epoch 100 -per 100 -w_pos -batch_size 128  -lr 0.00245 -weight_decay 0.1  -ES_pat 50 -wandb"
+COMMON="    -epoch 1 -per 100 -w_pos -batch_size 128  -lr 0.00245 -weight_decay 0.1  -ES_pat 50 -wandb"
 COEFS="-w_sample_label 10000  -w_time 1 -w_event 1"
  
 
@@ -57,8 +57,8 @@ do
     python Main.py  $COEFS $SETTING $COMMON $TEDA__shpmark -user_prefix "[$USER_PREFIX]" &
 
 
-    waitforjobs $N_JOBS
-    python Main.py  $COEFS $SETTING $COMMON $TEDA__ml -user_prefix "[$USER_PREFIX]" &
+    # waitforjobs $N_JOBS
+    # python Main.py  $COEFS $SETTING $COMMON $TEDA__ml -user_prefix "[$USER_PREFIX]" &
 
 
     waitforjobs $N_JOBS
@@ -71,6 +71,46 @@ do
     
 done
 
+
+
+
+DATA_NAME="p12"
+COMMON="    -epoch 1 -per 100 -w_pos -batch_size 128  -lr 0.00245 -weight_decay 0.1  -ES_pat 50 -wandb"
+COEFS="-w_sample_label 10000  -w_time 1 -w_event 1"
+ 
+
+for i_hosp in {0..2}
+do
+    for i_split in {0..4}
+    do
+
+    SETTING=" -data  $PRE/$DATA_NAME/ -setting mc1 -test_center $i_hosp -split $i_split " 
+    
+    # echo $SETTING
+
+
+
+    waitforjobs $N_JOBS
+    python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]"  &
+
+
+    waitforjobs $N_JOBS
+    python Main.py  $COEFS $SETTING $COMMON $TEDA__shpmark -user_prefix "[$USER_PREFIX]" &
+
+
+    # waitforjobs $N_JOBS
+    # python Main.py  $COEFS $SETTING $COMMON $TEDA__ml -user_prefix "[$USER_PREFIX]" &
+
+
+    waitforjobs $N_JOBS
+    python Main.py  $COEFS $SETTING $COMMON $DA__label -user_prefix "[$USER_PREFIX]" &
+
+    waitforjobs $N_JOBS
+    python Main.py  $COEFS $SETTING $COMMON $TEDA__label -user_prefix "[$USER_PREFIX]" &
+
+    done
+    
+done
 
 # # DA__label
 # # EXP=" -data  $PRE/$DATA_NAME/ -setting mc1 -test_center 0 -split 0 " 
