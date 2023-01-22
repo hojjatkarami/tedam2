1b8ffa6471401ecfa2bd5aac3a1b16a32684a606
hojjatkarami <g.hojatkarami@gmail.com>
2023-01-22 23:05:27 +0100

    up sh

--- runai_settings.sh
+++ runai_settings.sh
@@ -6,7 +6,7 @@ waitforjobs() {
 
 
 N_JOBS=6
-USER_PREFIX=V1
+USER_PREFIX=V3
 # p12     -lr 0.001 -weight_decay 0.001  
 # p19     -lr 0.001 -weight_decay 1    #DA__label   TE__shpmark
 
@@ -34,16 +34,16 @@ TEDA__ml="-event_enc 1 -state       -mod ml    -next_mark 0  -sample_label 0"
 
 
 DATA_NAME="p19"
-COMMON="    -epoch 100 -per 100 -w_pos -batch_size 128  -lr 0.00245 -weight_decay 0.1  -ES_pat 50 -wandb"
+COMMON="    -epoch 1 -per 100 -w_pos -batch_size 128  -lr 0.00245 -weight_decay 0.1  -ES_pat 50 -wandb"
 COEFS="-w_sample_label 10000  -w_time 1 -w_event 1"
  
 
 for i_hosp in {0..1}
 do
-    for i_split in {0..4}
+    for i_split in {0..0}
     do
 
-    SETTING=" -data  $PRE/$DATA_NAME/ -setting sc -test_center $i_hosp -split $i_split " 
+    SETTING=" -data  $PRE/$DATA_NAME/ -setting mc2 -test_center $i_hosp -split $i_split " 
     
     # echo $SETTING
 
@@ -75,16 +75,16 @@ done
 
 
 DATA_NAME="p12"
-COMMON="    -epoch 100 -per 100 -w_pos -batch_size 128  -lr 0.00245 -weight_decay 0.1  -ES_pat 50 -wandb"
+COMMON="    -epoch 1 -per 100 -w_pos -batch_size 128  -lr 0.00245 -weight_decay 0.1  -ES_pat 50 -wandb"
 COEFS="-w_sample_label 10000  -w_time 1 -w_event 1"
  
 
 for i_hosp in {0..2}
 do
-    for i_split in {0..4}
+    for i_split in {0}
     do
 
-    SETTING=" -data  $PRE/$DATA_NAME/ -setting sc -test_center $i_hosp -split $i_split " 
+    SETTING=" -data  $PRE/$DATA_NAME/ -setting mc2 -test_center $i_hosp -split $i_split " 
     
     # echo $SETTING
 
