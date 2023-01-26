6595b02ac84c45a7ad54d0e0d6333596a14ce376
hojjatkarami <g.hojatkarami@gmail.com>
2023-01-26 19:01:42 +0100

    sh up

--- runai_so.sh
+++ runai_so.sh
@@ -78,8 +78,8 @@ do
     COMMON=" -w_pos -epoch 100 -per 100  -batch_size 128  -lr 0.003 -weight_decay 0.1  -ES_pat 100 -wandb  -time_enc concat "
     python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" &
 
-    # COMMON="   -epoch 100 -per 100 -batch_size 64  -lr 0.0003 -weight_decay 0.1  -ES_pat 100 -wandb  -time_enc sum "
-    # python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" &
+    COMMON="   -epoch 100 -per 100 -batch_size 64  -lr 0.0003 -weight_decay 0.1  -ES_pat 100 -wandb  -time_enc sum "
+    python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" &
 
     # waitforjobs $N_JOBS
     # python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]"  &
