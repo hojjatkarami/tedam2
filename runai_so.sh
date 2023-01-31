08c30d5884dff1db307f2a33fa94db6f51618295
hojjatkarami <g.hojatkarami@gmail.com>
2023-01-31 22:02:46 +0100

    sh up

--- runai_so.sh
+++ runai_so.sh
@@ -37,25 +37,8 @@ COEFS="-w_sample_label 10000  -w_time 1 -w_event 1"
 
 
 
-# DATA_NAME="data_so"
-# COMMON="  -data_label multiclass  -epoch 3 -per 100  -batch_size 8  -lr 0.0003 -weight_decay 0.1  -ES_pat 100 -wandb "
-
- 
-# for i_split in {0..0}
-# do
-
-#     SETTING=" -data  $PRE/$DATA_NAME/ -split $i_split " 
-
-#     python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" -time_enc concat &
-
-#     python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" -time_enc sum &
-
-
-# done
-    
-
-DATA_NAME="synthea_full"
-COMMON="  -data_label multilabel  -epoch 3 -per 100  -batch_size 64  -lr 0.0003 -weight_decay 0.1  -ES_pat 100 -wandb "
+DATA_NAME="data_so"
+COMMON="  -data_label multiclass  -epoch 3 -per 100  -batch_size 8  -lr 0.003 -weight_decay 0.1  -ES_pat 100 -wandb "
 
  
 for i_split in {0..0}
@@ -69,9 +52,26 @@ do
 
 
 done
+    
+
+# DATA_NAME="synthea_full"
+# COMMON="  -data_label multilabel  -epoch 3 -per 100  -batch_size 64  -lr 0.003 -weight_decay 0.1  -ES_pat 100 -wandb "
+
+ 
+# for i_split in {0..0}
+# do
+
+#     SETTING=" -data  $PRE/$DATA_NAME/ -split $i_split " 
+
+#     python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" -time_enc concat &
+
+#     python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" -time_enc sum &
+
+
+# done
 
 # DATA_NAME="retweets"
-# COMMON="  -data_label multilabel  -epoch 3 -per 100  -batch_size 256  -lr 0.0003 -weight_decay 0.1  -ES_pat 100 -wandb "
+# COMMON="  -data_label multilabel  -epoch 3 -per 100  -batch_size 256  -lr 0.003 -weight_decay 0.1  -ES_pat 100 -wandb "
 
  
 # for i_split in {0..0}
