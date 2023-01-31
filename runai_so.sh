472f5570bf921b037b91995cae997b86787492db
hojjatkarami <g.hojatkarami@gmail.com>
2023-01-31 21:03:40 +0100

    sh up

--- runai_so.sh
+++ runai_so.sh
@@ -17,9 +17,7 @@ PRE="C:/DATA/data/processed"
 PRE="/scratch/hokarami/new"
 # PRE="/scratch/hokarami/data_old"
 
-#p12_full_seft,  p12_full_hosp,                  physio2019_1d_HP_std_AB,          physio2019_1d_HP_std_rand
-# EXP="  -setting mc2 -test_center 0 "
-# declare -i REP=3
+
 
 
 DA__label="-event_enc 0 -state       -mod none    -next_mark 0  -sample_label 1 "
@@ -35,51 +33,31 @@ TEDA__shpmark="-event_enc 1    -state       -mod single    -next_mark 1  -sample
 TEDA__shp="-event_enc 1 -state       -mod single    -next_mark 0  -sample_label 0"
 TEDA__ml="-event_enc 1 -state       -mod ml    -next_mark 0  -sample_label 0"
 
-
-
-
-DATA_NAME="retweets"
-
 COEFS="-w_sample_label 10000  -w_time 1 -w_event 1"
+
+
+
+DATA_NAME="data_so"
+COMMON="  -data_label multiclass  -epoch 30 -per 100  -batch_size 8  -lr 0.0003 -weight_decay 0.1  -ES_pat 100 -wandb "
+
  
 
-
-SETTING=" -data  $PRE/$DATA_NAME/ -split '' -data_label multiclass" 
-
-COMMON="   -epoch 30 -per 100  -batch_size 8  -lr 0.0003 -weight_decay 0.1  -ES_pat 100 -wandb  -time_enc concat "
-# python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" &
-
-COMMON="   -epoch 30 -per 100 -batch_size 8  -lr 0.0003 -weight_decay 0.1  -ES_pat 100 -wandb  -time_enc sum "
-# python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" &
-
-
-# COMMON="   -epoch 30 -per 100  -batch_size 8  -lr 0.0003 -weight_decay 0.1  -ES_pat 100 -wandb  -time_enc concat "
-# python Main.py  $COEFS $SETTING $COMMON $TE__markmc -user_prefix "[$USER_PREFIX]" &
-
-# COMMON="   -epoch 30 -per 100 -batch_size 8  -lr 0.0003 -weight_decay 0.1  -ES_pat 100 -wandb  -time_enc sum "
-# python Main.py  $COEFS $SETTING $COMMON $TE__markmc -user_prefix "[$USER_PREFIX]" &
-
-# python Main.py  $COEFS $SETTING $COMMON $TEDA__label -user_prefix "[$USER_PREFIX]" &
-# python Main.py  $COEFS $SETTING $COMMON $TEDA__label -user_prefix "[$USER_PREFIX]" &
-# python Main.py  $COEFS $SETTING $COMMON $TEDA__label -user_prefix "[$USER_PREFIX]" &
-# python Main.py  $COEFS $SETTING $COMMON $TEDA__label -user_prefix "[$USER_PREFIX]" &
 
 
 
 for i_split in {0..0}
 do
 
-    SETTING=" -data  $PRE/$DATA_NAME/ -split $i_split -data_label multiclass" 
-    SETTING=" -data  $PRE/$DATA_NAME/                 -data_label multiclass" 
-    SETTING=" -data  $PRE/$DATA_NAME/ -split $i_split -data_label multilabel" 
+    SETTING=" -data  $PRE/$DATA_NAME/ -split $i_split " 
+    # SETTING=" -data  $PRE/$DATA_NAME/                 -data_label multiclass" 
+    # SETTING=" -data  $PRE/$DATA_NAME/ -split $i_split -data_label multilabel" 
 
     # echo $SETTING
 
-    COMMON="  -epoch 100 -per 100  -batch_size 8  -lr 0.003 -weight_decay 0.1  -ES_pat 100 -wandb  -time_enc concat "
-    echo python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" &
 
-    # COMMON="  -w_pos -epoch 100 -per 100 -batch_size 256  -lr 0.003 -weight_decay 0.1  -ES_pat 100 -wandb  -time_enc sum "
-    # python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" &
+    python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" -time_enc concat &
+
+    python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" -time_enc sum &
 
     # waitforjobs $N_JOBS
     # python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]"  &
