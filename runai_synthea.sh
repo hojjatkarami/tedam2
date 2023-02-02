2f98b8a30d0f3dde4c6e8e91181362629ad63095
hojjatkarami <g.hojatkarami@gmail.com>
2023-02-02 18:33:44 +0100

    sd

--- runai_synthea.sh
+++ runai_synthea.sh
@@ -4,8 +4,8 @@ waitforjobs() {
 }
 
 
-N_JOBS=2
-USER_PREFIX=R4-FULL-lr03
+N_JOBS=1
+USER_PREFIX=R4-FULL2
 # p12     -lr 0.001 -weight_decay 0.001  
 # p19     -lr 0.001 -weight_decay 1    #DA__label   TE__shpmark
 
@@ -40,8 +40,8 @@ COEFS="-w_sample_label 10000  -w_time 1 -w_event 1"
     
 
 DATA_NAME="synthea_full"
-COMMON=" -w_pos -pos_alpha 1 -data_label multilabel  -epoch 100 -per 100  -batch_size 64  -lr 0.03   -ES_pat 100 -wandb "
-HPs="-te_d_mark 32 -te_d_time 16 -te_d_inner 128 -te_d_k 32 -te_d_v 32 "
+COMMON=" -w_pos -pos_alpha 1 -data_label multilabel  -epoch 30 -per 100  -batch_size 64  -lr 0.003   -ES_pat 100 -wandb "
+HPs="-te_d_mark 128 -te_d_time 64 -te_d_inner 512 -te_d_k 64 -te_d_v 64 "
  
 for i_split in {1..4}
 do
@@ -65,8 +65,7 @@ do
     waitforjobs $N_JOBS
     python Main.py $HPs $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" -time_enc concat  -weight_decay 1 &
 
-    waitforjobs $N_JOBS
-    python Main.py $HPs $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX wd10]" -time_enc concat  -weight_decay 10 &
+
 
     # waitforjobs $N_JOBS
     # python Main.py  $COEFS $SETTING $COMMON $TE__shpmark -user_prefix "[$USER_PREFIX]" -time_enc concat  &
