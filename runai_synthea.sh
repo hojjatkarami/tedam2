3df19124621ff7188932b414ecfdcedb5649fc42
hojjatkarami <g.hojatkarami@gmail.com>
2023-02-07 19:06:48 +0100

    sh up

--- runai_synthea.sh
+++ runai_synthea.sh
@@ -5,7 +5,7 @@ waitforjobs() {
 
 
 N_JOBS=2
-USER_PREFIX=R5-OLD-100var-NoEvent-sum
+USER_PREFIX=R6
 # p12     -lr 0.001 -weight_decay 0.001  
 # p19     -lr 0.001 -weight_decay 1    #DA__label   TE__shpmark
 
