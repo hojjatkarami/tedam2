8cdb8b865735a4dfc3e762559862bf32d6f0f205
hojjatkarami <g.hojatkarami@gmail.com>
2023-02-14 18:49:45 +0100

    sh up

--- runai_event_p12.sh
+++ runai_event_p12.sh
@@ -5,7 +5,7 @@ waitforjobs() {
 
 N_JOBS=4
 
-USER_PREFIX=C40-noDemo-
+USER_PREFIX=C40
 
 DATA_NAME="p12"
 COMMON=" -data_label multilabel  -epoch 50 -per 100    -ES_pat 100 -wandb -wandb_project TEEDAM_unsupervised "
@@ -20,9 +20,9 @@ TE__nextmark="-event_enc 1          -mod none      -next_mark 1     -mark_detach
 TE__pp_single_mark="-event_enc 1          -mod single    -next_mark 1     -mark_detach 0      -sample_label 0"
 TE__pp_ml="-event_enc 1          -mod ml        -next_mark 1     -mark_detach 1      -sample_label 0"
 # without label + DAM
-TEDA__nextmark="-event_enc 1    -state          -mod none      -next_mark 1     -mark_detach 0      -sample_label 0"
-TEDA__pp_single_mark="-event_enc 1    -state          -mod single    -next_mark 1     -mark_detach 0      -sample_label 0"
-TEDA__pp_ml="-event_enc 1    -state          -mod ml        -next_mark 1     -mark_detach 1      -sample_label 0"
+TEDA__nextmark="-event_enc 1    -state -demo          -mod none      -next_mark 1     -mark_detach 0      -sample_label 0"
+TEDA__pp_single_mark="-event_enc 1    -state -demo          -mod single    -next_mark 1     -mark_detach 0      -sample_label 0"
+TEDA__pp_ml="-event_enc 1    -state -demo          -mod ml        -next_mark 1     -mark_detach 1      -sample_label 0"
 # without label + noise
 TEnoise__nextmark="-event_enc 1      -noise          -mod none      -next_mark 1     -mark_detach 0      -sample_label 0"
 TEnoise__pp_single_mark="-event_enc 1      -noise          -mod single    -next_mark 1     -mark_detach 0      -sample_label 0"
@@ -99,52 +99,76 @@ COEFS="-w_sample_label 100  -w_time 1 -w_event 1"
 
 
 
-# # random split (rand)
-# SETTING=" -data  $PRE/$DATA_NAME/ -setting rand "     
+# seft split (seft)
+SETTING=" -data  $PRE/$DATA_NAME/ -setting seft "     
 
-#     # TE__pp_ml
-#     waitforjobs $N_JOBS
-#     python Main.py  $HPs $COEFS $SETTING $COMMON $TE__pp_ml -user_prefix "[$USER_PREFIX-TE__pp_ml-concat]" -time_enc concat &    
+    # TE__pp_ml
+    waitforjobs $N_JOBS
+    python Main.py  $HPs $COEFS $SETTING $COMMON $TE__pp_ml -user_prefix "[$USER_PREFIX-TE__pp_ml-concat]" -time_enc concat &    
 
-#     # TEDA__pp_ml
-#     waitforjobs $N_JOBS
-#     python Main.py  $HPs $COEFS $SETTING $COMMON $TEDA__pp_ml -user_prefix "[$USER_PREFIX-TEDA__pp_ml-concat]" -time_enc concat &    
-
-
-#     # TEnoise__pp_ml
-#     waitforjobs $N_JOBS
-#     python Main.py  $HPs $COEFS $SETTING $COMMON $TEnoise__pp_ml -user_prefix "[$USER_PREFIX-TEnoise__pp_ml-concat]" -time_enc concat &    
+    # TEDA__pp_ml
+    waitforjobs $N_JOBS
+    python Main.py  $HPs $COEFS $SETTING $COMMON $TEDA__pp_ml -user_prefix "[$USER_PREFIX-TEDA__pp_ml-concat]" -time_enc concat &    
 
 
+    # TEnoise__pp_ml
+    waitforjobs $N_JOBS
+    python Main.py  $HPs $COEFS $SETTING $COMMON $TEnoise__pp_ml -user_prefix "[$USER_PREFIX-TEnoise__pp_ml-concat]" -time_enc concat &    
 
-# # multi-center external evaluation split (mc2)    
+
+
+# multi-center external evaluation split (mc2)    
+for i_hosp in {0..2}
+do
+    SETTING=" -data  $PRE/$DATA_NAME/ -setting mc2 -test_center $i_hosp " 
+
+        # TE__pp_ml
+        waitforjobs $N_JOBS
+        python Main.py  $HPs $COEFS $SETTING $COMMON $TE__pp_ml -user_prefix "[$USER_PREFIX-TE__pp_ml-concat]" -time_enc concat &    
+
+        # TEDA__pp_ml
+        waitforjobs $N_JOBS
+        python Main.py  $HPs $COEFS $SETTING $COMMON $TEDA__pp_ml -user_prefix "[$USER_PREFIX-TEDA__pp_ml-concat]" -time_enc concat &    
+
+
+        # TEnoise__pp_ml
+        waitforjobs $N_JOBS
+        python Main.py  $HPs $COEFS $SETTING $COMMON $TEnoise__pp_ml -user_prefix "[$USER_PREFIX-TEnoise__pp_ml-concat]" -time_enc concat &    
+
+done
+
+
+
+# # multi-center split (mc1)    
 # for i_hosp in {0..2}
 # do
-#     SETTING=" -data  $PRE/$DATA_NAME/ -setting mc2 -test_center $i_hosp " 
+#     for i_split in {0..4}
+#     do
+#         SETTING=" -data  $PRE/$DATA_NAME/ -setting mc1 -test_center $i_hosp -split $i_split " 
 
-#         # TE__pp_ml
-#         waitforjobs $N_JOBS
-#         python Main.py  $HPs $COEFS $SETTING $COMMON $TE__pp_ml -user_prefix "[$USER_PREFIX-TE__pp_ml-concat]" -time_enc concat &    
+#             # TE__pp_ml
+#             waitforjobs $N_JOBS
+#             python Main.py  $HPs $COEFS $SETTING $COMMON $TE__pp_ml -user_prefix "[$USER_PREFIX-TE__pp_ml-concat]" -time_enc concat &    
 
-#         # TEDA__pp_ml
-#         waitforjobs $N_JOBS
-#         python Main.py  $HPs $COEFS $SETTING $COMMON $TEDA__pp_ml -user_prefix "[$USER_PREFIX-TEDA__pp_ml-concat]" -time_enc concat &    
+#             # TEDA__pp_ml
+#             waitforjobs $N_JOBS
+#             python Main.py  $HPs $COEFS $SETTING $COMMON $TEDA__pp_ml -user_prefix "[$USER_PREFIX-TEDA__pp_ml-concat]" -time_enc concat &    
 
 
-#         # TEnoise__pp_ml
-#         waitforjobs $N_JOBS
-#         python Main.py  $HPs $COEFS $SETTING $COMMON $TEnoise__pp_ml -user_prefix "[$USER_PREFIX-TEnoise__pp_ml-concat]" -time_enc concat &    
-
+#             # TEnoise__pp_ml
+#             waitforjobs $N_JOBS
+#             python Main.py  $HPs $COEFS $SETTING $COMMON $TEnoise__pp_ml -user_prefix "[$USER_PREFIX-TEnoise__pp_ml-concat]" -time_enc concat &    
+#     done
 # done
 
 
 
-# multi-center split (mc1)    
+# single-center split (sc)    
 for i_hosp in {0..2}
 do
     for i_split in {0..4}
     do
-        SETTING=" -data  $PRE/$DATA_NAME/ -setting mc1 -test_center $i_hosp -split $i_split " 
+        SETTING=" -data  $PRE/$DATA_NAME/ -setting sc -test_center $i_hosp -split $i_split " 
 
             # TE__pp_ml
             waitforjobs $N_JOBS
@@ -160,4 +184,3 @@ do
             python Main.py  $HPs $COEFS $SETTING $COMMON $TEnoise__pp_ml -user_prefix "[$USER_PREFIX-TEnoise__pp_ml-concat]" -time_enc concat &    
     done
 done
-
