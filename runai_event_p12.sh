7b406764f60cc674112ace72caf02039773ae219
hojjatkarami <g.hojatkarami@gmail.com>
2023-02-14 11:13:26 +0100

    sh up add demo

--- runai_event_p12.sh
+++ runai_event_p12.sh
@@ -8,7 +8,7 @@ N_JOBS=4
 USER_PREFIX=R40
 
 DATA_NAME="p12"
-COMMON=" -data_label multilabel  -epoch 50 -per 100    -ES_pat 100 -wandb "
+COMMON=" -data_label multilabel  -epoch 50 -per 100    -ES_pat 100 -wandb -wandb_project TEEDAM_unsupervised "
 HPs="-batch_size 128  -lr 0.00245 -weight_decay 0.1 -te_d_mark 32 -te_d_time 16 -te_d_inner 128 -te_d_k 32 -te_d_v 32 "
 
 
--- runai_label_p12.sh
+++ runai_label_p12.sh
@@ -5,10 +5,10 @@ waitforjobs() {
 
 N_JOBS=4
 
-USER_PREFIX=R40
+USER_PREFIX=R40-demo
 
 DATA_NAME="p12"
-COMMON=" -data_label multilabel  -epoch 50 -per 100    -ES_pat 100 -wandb -wandb_project TEEDAM_supervised "
+COMMON=" -demo -data_label multilabel  -epoch 50 -per 100    -ES_pat 100 -wandb -wandb_project TEEDAM_supervised "
 HPs="-batch_size 128  -lr 0.00245 -weight_decay 0.1 -te_d_mark 32 -te_d_time 16 -te_d_inner 128 -te_d_k 32 -te_d_v 32 "
 
 
@@ -29,16 +29,16 @@ DA__pp_ml="-event_enc 0    -state          -mod ml        -next_mark 1     -mark
 COEFS="-w_sample_label 10000  -w_time 1 -w_event 1"
 
 
-# random split (rand)
-SETTING=" -data  $PRE/$DATA_NAME/ -setting rand "     
+# # random split (rand)
+# SETTING=" -data  $PRE/$DATA_NAME/ -setting rand "     
 
-    # TEDA__pp_single_mark
-    waitforjobs $N_JOBS
-    python Main.py  $HPs $COEFS $SETTING $COMMON $TEDA__pp_single_mark -user_prefix "[$USER_PREFIX-TEDA__pp_single_mark-concat ]" -time_enc concat &    
+#     # TEDA__pp_single_mark
+#     waitforjobs $N_JOBS
+#     python Main.py  $HPs $COEFS $SETTING $COMMON $TEDA__pp_single_mark -user_prefix "[$USER_PREFIX-TEDA__pp_single_mark-concat ]" -time_enc concat &    
 
-    # DA__pp_single_mark
-    waitforjobs $N_JOBS
-    python Main.py  $HPs $COEFS $SETTING $COMMON $DA__pp_single_mark -user_prefix "[$USER_PREFIX-DA__pp_single_mark-concat ]" -time_enc concat &    
+#     # DA__pp_single_mark
+#     waitforjobs $N_JOBS
+#     python Main.py  $HPs $COEFS $SETTING $COMMON $DA__pp_single_mark -user_prefix "[$USER_PREFIX-DA__pp_single_mark-concat ]" -time_enc concat &    
 
 
 
