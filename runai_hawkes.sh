9aa535985d9bc3bf51afdd6cd3463a3e2dfa13be
hojjatkarami <g.hojatkarami@gmail.com>
2023-03-29 10:46:43 +0200

    sh up

---
+++ runai_hawkes.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+waitforjobs() {
+    while test $(jobs -p | wc -w) -ge "$1"; do wait -n; done
+}
+
+N_JOBS=2
+
+USER_PREFIX=H70
+
+DATA_NAME="data_hawkes"
+COMMON=" -data_label multiclass  -epoch 50 -per 100    -ES_pat 100 -wandb -wandb_project TEEDAM_unsupervised_timeCat "
+HPs="-batch_size 32  -lr 0.001 -weight_decay 0.1 -te_d_mark 32 -te_d_time 16 -te_d_inner 128 -te_d_k 32 -te_d_v 32 "
+
+
+PRE="/scratch/hokarami/data_old"
+PRE="/mlodata1/hokarami/tedam"
+
+# without label
+TE__nextmark="-event_enc 1          -mod none      -next_mark 1     -mark_detach 0      -sample_label 0"
+TE__pp_single_mark="-event_enc 1          -mod single    -next_mark 1     -mark_detach 0      -sample_label 0"
+TE__pp_mc="-event_enc 1          -mod mc    -next_mark 1     -mark_detach 1      -sample_label 0"
+TE__pp_ml="-event_enc 1          -mod ml        -next_mark 1     -mark_detach 1      -sample_label 0"
+
+COEFS="-w_sample_label 10000  -w_time 1 -w_event 1"
+
+for i_diag in {-2..1}
+do
+
+
+
+    SETTING=" -data  $PRE/$DATA_NAME/  -diag_offset $i_diag " 
+        
+    
+    waitforjobs $N_JOBS
+    python Main.py  $HPs $COEFS $SETTING $COMMON $TE__nextmark -user_prefix "[$USER_PREFIX-TE__nextmark-concat-d$i_diag]" -time_enc concat &
+    
+
+    # waitforjobs $N_JOBS
+    # python Main.py  $HPs $COEFS $SETTING $COMMON $TE__nextmark -user_prefix "[$USER_PREFIX-TE__nextmark-sum-d$i_diag]" -time_enc sum &
+
+
+
+
+    
+    # waitforjobs $N_JOBS
+    # python Main.py  $HPs $COEFS $SETTING $COMMON $TE__pp_single_mark -user_prefix "[$USER_PREFIX-TE__pp_single_mark-concat-d$i_diag]" -time_enc concat &
+    
+
+    # waitforjobs $N_JOBS
+    # python Main.py  $HPs $COEFS $SETTING $COMMON $TE__pp_single_mark -user_prefix "[$USER_PREFIX-TE__pp_single_mark-sum-d$i_diag]" -time_enc sum &
+    
+
+
+    
+    # waitforjobs $N_JOBS
+    # python Main.py  $HPs $COEFS $SETTING $COMMON $TE__pp_mc -user_prefix "[$USER_PREFIX-TE__pp_mc-concat-d$i_diag]" -time_enc concat &
+    
+
+    # waitforjobs $N_JOBS
+    # python Main.py  $HPs $COEFS $SETTING $COMMON $TE__pp_mc -user_prefix "[$USER_PREFIX-TE__pp_mc-sum-d$i_diag]" -time_enc sum &
+
+    
+done
+
+
+
--- runai_label_p19_tl.sh
+++ runai_label_p19_tl.sh
@@ -61,39 +61,39 @@ i_diag=0
 
 
 
-# multi-center split (mc1)    
-for i_hosp in {0..1}
-do
-    for i_split in {0..4}
-    do
-        SETTING=" -diag_offset $i_diag -data  $PRE/$DATA_NAME/ -setting mc1 -test_center $i_hosp -split $i_split " 
-        TL='-transfer_learning DO '
+# # multi-center split (mc1)    
+# for i_hosp in {0..1}
+# do
+#     for i_split in {0..4}
+#     do
+#         SETTING=" -diag_offset $i_diag -data  $PRE/$DATA_NAME/ -setting mc1 -test_center $i_hosp -split $i_split " 
+#         TL='-transfer_learning DO '
 
-        # DA__base
-        waitforjobs $N_JOBS
-        python Main.py  $HPs $COEFS $SETTING $COMMON $DA__base -user_prefix "[$USER_PREFIX-DA__base-concat]" -time_enc concat &    
+#         # DA__base
+#         waitforjobs $N_JOBS
+#         python Main.py  $HPs $COEFS $SETTING $COMMON $DA__base -user_prefix "[$USER_PREFIX-DA__base-concat]" -time_enc concat &    
 
             
-        # TEDA__none
-        waitforjobs $N_JOBS
-        python Main.py  $TL $HPs $COEFS $SETTING $COMMON $TEDA__none -user_prefix "[$USER_PREFIX-TEDA__none-concat]" -time_enc concat &   
+#         # TEDA__none
+#         waitforjobs $N_JOBS
+#         python Main.py  $TL $HPs $COEFS $SETTING $COMMON $TEDA__none -user_prefix "[$USER_PREFIX-TEDA__none-concat]" -time_enc concat &   
 
 
-    done
-done
+#     done
+# done
 
 
 # single-center split (sc)    
-for i_hosp in {0..1}
+for i_hosp in {1..1}
 do
     for i_split in {0..4}
     do
         SETTING=" -diag_offset $i_diag -data  $PRE/$DATA_NAME/ -setting sc -test_center $i_hosp -split $i_split " 
         TL='-transfer_learning DO '
 
-        # DA__base
-        waitforjobs $N_JOBS
-        python Main.py  $HPs $COEFS $SETTING $COMMON $DA__base -user_prefix "[$USER_PREFIX-DA__base-concat]" -time_enc concat &    
+        # # DA__base
+        # waitforjobs $N_JOBS
+        # python Main.py  $HPs $COEFS $SETTING $COMMON $DA__base -user_prefix "[$USER_PREFIX-DA__base-concat]" -time_enc concat &    
 
             
         # TEDA__none
