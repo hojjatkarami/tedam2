0c898c9f703c0438b213c5164cbdadde809c9612
hojjatkarami <g.hojatkarami@gmail.com>
2023-04-17 11:57:41 +0200

    sh up

--- runai_label_p19_tl2.sh
+++ runai_label_p19_tl2.sh
@@ -9,7 +9,7 @@ USER_PREFIX=H70HHGTEST1-
 
 DATA_NAME="p19"
 COMMON=" -data_label multilabel  -epoch 50 -per 100    -ES_pat 100 -wandb -wandb_project TEEDAM_supervised "
-HPs="-batch_size 128  -lr 0.01 -weight_decay 0.1 -te_d_mark 32 -te_d_time 16 -te_d_inner 128 -te_d_k 32 -te_d_v 32 "
+HPs="-batch_size 128  -lr 0.001 -weight_decay 0.1 -te_d_mark 32 -te_d_time 16 -te_d_inner 128 -te_d_k 32 -te_d_v 32 "
 
 
 
