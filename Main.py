967c17d1aeebe09a62b7fe8755d619b55963bf55
hojjatkarami <g.hojatkarami@gmail.com>
2023-05-16 14:23:41 +0200

    more complex DAM

--- Main.py
+++ Main.py
@@ -1257,7 +1257,7 @@ def config(opt, justLoad=False):
     if opt.state:
 
         opt.DAM_config['output_activation'] = 'relu'
-        opt.DAM_config['output_dims'] = 4
+        opt.DAM_config['output_dims'] = 8  # 4
 
         # MLP encoder for combined values
         opt.DAM_config['n_phi_layers'] = 3
@@ -1266,20 +1266,20 @@ def config(opt, justLoad=False):
 
         # Cumulative Set Attention Layer
         opt.DAM_config['n_psi_layers'] = 2
-        opt.DAM_config['psi_width'] = 16
+        opt.DAM_config['psi_width'] = 32  # 16
         opt.DAM_config['psi_latent_width'] = 32
 
-        opt.DAM_config['dot_prod_dim'] = 16
-        opt.DAM_config['n_heads'] = 2
+        opt.DAM_config['dot_prod_dim'] = 32  # 16
+        opt.DAM_config['n_heads'] = 4  # 2
         opt.DAM_config['attn_dropout'] = 0.8
         opt.DAM_config['latent_width'] = 16
 
         opt.DAM_config['n_rho_layers'] = 2
-        opt.DAM_config['rho_width'] = 32  # 256
+        opt.DAM_config['rho_width'] = 32  #
         opt.DAM_config['rho_dropout'] = 0.8
 
         opt.DAM_config['max_timescale'] = 1000
-        opt.DAM_config['n_positional_dims'] = 4
+        opt.DAM_config['n_positional_dims'] = 8  # 4
         opt.DAM_config['num_mods'] = opt.num_states
         opt.DAM_config['num_demos'] = opt.num_demos
         opt.DAM_config['online'] = False
