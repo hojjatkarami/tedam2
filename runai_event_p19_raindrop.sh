6d88f9252bd934e049d4165d89ad0ddb6427a793
hojjatkarami <g.hojatkarami@gmail.com>
2023-06-07 16:35:06 +0200

    droput out predict 0.7

--- runai_event_p19_raindrop.sh
+++ runai_event_p19_raindrop.sh
@@ -5,7 +5,7 @@ waitforjobs() {
 
 N_JOBS=2    
 
-USER_PREFIX=RD-simpler2-SAHPxavier-PredictorDO02-
+USER_PREFIX=RD-simpler2-SAHPxavier-PredictorDO07-
 
 DATA_NAME="p19"
 COMMON=" -data_label multilabel  -epoch 100 -per 100    -ES_pat 100 -wandb -wandb_project TEEDAM_unsupervised "
