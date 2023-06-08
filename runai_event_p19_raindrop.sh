298022c7ce15d7b98a9435bb39c9977506598349
hojjatkarami <g.hojatkarami@gmail.com>
2023-06-08 09:15:36 +0200

    RD74 unsupervised

--- runai_event_p19_raindrop.sh
+++ runai_event_p19_raindrop.sh
@@ -3,12 +3,13 @@ waitforjobs() {
     while test $(jobs -p | wc -w) -ge "$1"; do wait -n; done
 }
 
-N_JOBS=2    
+N_JOBS=5    
 
-USER_PREFIX=RD-simpler2-SAHPxavier-LRhigher-bs64-
+# USER_PREFIX=RD-simpler2-SAHPxavier-LRhigher-bs64-
+USER_PREFIX=RD74
 
 DATA_NAME="p19"
-COMMON=" -data_label multilabel  -epoch 100 -per 100    -ES_pat 100 -wandb -wandb_project TEEDAM_unsupervised "
+COMMON=" -data_label multilabel  -epoch 50 -per 100    -ES_pat 100 -wandb -wandb_project TEEDAM_unsupervised "
 HPs="-batch_size 128  -lr 0.01 -weight_decay 0.1 -te_d_mark 32 -te_d_time 16 -te_d_inner 128 -te_d_k 32 -te_d_v 32 "  # old
 HPs="-batch_size 64  -lr 0.01 -weight_decay 1 -te_d_mark 8 -te_d_time 4 -te_d_inner 32 -te_d_k 8 -te_d_v 8" # simpler2
 # HPs="-batch_size 64  -lr 0.01 -weight_decay 1 -te_d_mark 16 -te_d_time 8 -te_d_inner 64 -te_d_k 16 -te_d_v 16" # simpler
@@ -41,11 +42,6 @@ COEFS="-w_sample_label 100  -w_time 1 -w_event 1"
 for i_diag in {0..0}
 do
 
-
-
-
-
-
     for i_split in {0..4}
     do
         SETTING=" -diag_offset $i_diag -data  $PRE/$DATA_NAME/ -setting raindrop  -split $i_split " 
@@ -59,15 +55,9 @@ do
             python Main.py  $HPs $COEFS $SETTING $COMMON $TEDA__pp_single_mark -user_prefix "[$USER_PREFIX-TEDA__pp_single_mark-concat-d$i_diag]" -time_enc concat &    
 
 
-            # # # TEnoise__pp_single_mark
+            # # TEnoise__pp_single_mark
             # waitforjobs $N_JOBS
             # python Main.py  $HPs $COEFS $SETTING $COMMON $TEnoise__pp_single_mark -user_prefix "[$USER_PREFIX-TEnoise__pp_single_mark-concat-d$i_diag]" -time_enc concat &    
     done
-
-
-
-
-
-
 
 done # for i_diag
