b85f97ee216117687fe0b58a4e1ab5531c6989b9
hojjatkarami <g.hojatkarami@gmail.com>
2023-06-13 11:24:58 +0200

    sh up

--- runai_event_p19_raindrop.sh
+++ runai_event_p19_raindrop.sh
@@ -3,16 +3,16 @@ waitforjobs() {
     while test $(jobs -p | wc -w) -ge "$1"; do wait -n; done
 }
 
-N_JOBS=2    
+N_JOBS=5    
 
 # USER_PREFIX=RD-simpler2-SAHPxavier-LRhigher-bs64-
-USER_PREFIX=RD74
+USER_PREFIX=RD74-R2-
 
 DATA_NAME="p19"
 COMMON=" -data_label multilabel  -epoch 50 -per 100    -ES_pat 100 -wandb -wandb_project TEEDAM_unsupervised "
 HPs="-batch_size 128  -lr 0.01 -weight_decay 0.1 -te_d_mark 32 -te_d_time 16 -te_d_inner 128 -te_d_k 32 -te_d_v 32 "  # old
 HPs="-batch_size 64  -lr 0.005 -weight_decay 1 -te_d_mark 8 -te_d_time 4 -te_d_inner 32 -te_d_k 8 -te_d_v 8" # simpler2
-# HPs="-batch_size 64  -lr 0.01 -weight_decay 1 -te_d_mark 16 -te_d_time 8 -te_d_inner 64 -te_d_k 16 -te_d_v 16" # simpler
+HPs="-batch_size 64  -lr 0.005 -weight_decay 1 -te_d_mark 16 -te_d_time 8 -te_d_inner 64 -te_d_k 16 -te_d_v 16" # simpler
 
 
 
@@ -48,37 +48,13 @@ do
 
 
 
-HPs="-batch_size 64  -lr 0.005 -weight_decay 1 -te_d_mark 8 -te_d_time 4 -te_d_inner 32 -te_d_k 8 -te_d_v 8" # simpler2
-USER_PREFIX=RD74-lrLower
-
-# TE__pp_single_mark
+            # TE__pp_single_mark
             waitforjobs $N_JOBS
             python Main.py  $HPs $COEFS $SETTING $COMMON $TE__pp_single_mark -user_prefix "[$USER_PREFIX-TE__pp_single_mark-concat-d$i_diag]" -time_enc concat &    
 
             # TEDA__pp_single_mark
             waitforjobs $N_JOBS
             python Main.py  $HPs $COEFS $SETTING $COMMON $TEDA__pp_single_mark -user_prefix "[$USER_PREFIX-TEDA__pp_single_mark-concat-d$i_diag]" -time_enc concat &    
-
-
-HPs="-batch_size 64  -lr 0.005 -weight_decay 1 -te_d_mark 16 -te_d_time 8 -te_d_inner 64 -te_d_k 16 -te_d_v 16" # simpler
-USER_PREFIX=RD74-lrLower-complex
-
-            waitforjobs $N_JOBS
-            python Main.py  $HPs $COEFS $SETTING $COMMON $TE__pp_single_mark -user_prefix "[$USER_PREFIX-TE__pp_single_mark-concat-d$i_diag]" -time_enc concat &    
-
-            # TEDA__pp_single_mark
-            waitforjobs $N_JOBS
-            python Main.py  $HPs $COEFS $SETTING $COMMON $TEDA__pp_single_mark -user_prefix "[$USER_PREFIX-TEDA__pp_single_mark-concat-d$i_diag]" -time_enc concat &    
-
-
-
-            # # TE__pp_single_mark
-            # waitforjobs $N_JOBS
-            # python Main.py  $HPs $COEFS $SETTING $COMMON $TE__pp_single_mark -user_prefix "[$USER_PREFIX-TE__pp_single_mark-concat-d$i_diag]" -time_enc concat &    
-
-            # # TEDA__pp_single_mark
-            # waitforjobs $N_JOBS
-            # python Main.py  $HPs $COEFS $SETTING $COMMON $TEDA__pp_single_mark -user_prefix "[$USER_PREFIX-TEDA__pp_single_mark-concat-d$i_diag]" -time_enc concat &    
 
 
             # # TEnoise__pp_single_mark
